92e3a2a4cdfd66dc4945e68c310bb67d55ad460e
Rahman <mr185242@ncr.com>
2017-10-13 17:53:02 +0530

    styles

--- src/app/dot/dot.component.ts
+++ src/app/dot/dot.component.ts
@@ -2,7 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dot',
-  template: `<h1 class="cursor">&bull;</h1>`,
-  styles: []
+  template: `<h1 class="cursor" (click)="toggleMenu()">&bull;</h1>
+  
+  <div class="menu" *ngIf="showMenu">
+     <ul>
+        <li *ngFor="let item of menu">  
+          {{item}}
+        </li>
+     </ul>
+  </div>
+  `
 })
-export class DotComponent {}
+export class DotComponent {
+  menu: Array<string>;
+  showMenu: boolean = false;
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+  constructor() {
+    this.menu = ["Home", "About", "Contact"]
+  }
+}
